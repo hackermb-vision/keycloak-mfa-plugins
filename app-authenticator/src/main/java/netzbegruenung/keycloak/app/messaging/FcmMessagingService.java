525dfe6bd6517a5e068f875c5a35d2cec30a01f1
Moritz <moritz.geiger@rwu.de>
2025-05-22 16:03:53 +0200

    use ntfy instead of firebase

--- app-authenticator/src/main/java/netzbegruenung/keycloak/app/messaging/FcmMessagingService.java
+++ app-authenticator/src/main/java/netzbegruenung/keycloak/app/messaging/FcmMessagingService.java
@@ -1,11 +1,13 @@
 package netzbegruenung.keycloak.app.messaging;
 
+import ntfyJava.NtfyClient;
+import ntfyJava.core.model.*;
+import ntfyJava.core.publish.PubClient;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.firebase.messaging.FirebaseMessaging;
-import com.google.firebase.messaging.FirebaseMessagingException;
-import com.google.firebase.messaging.Message;
 import netzbegruenung.keycloak.app.dto.ChallengeDto;
 import org.jboss.logging.Logger;
+
 
 import java.net.URI;
 import java.util.Map;
@@ -21,17 +23,16 @@ public class FcmMessagingService implements MessagingService {
 			logger.infof("Skip sending firebase notification: missing device push ID user [%s]", challenge.getUserName());
 			return;
 		}
-		Map<String, String> challengeMap = objectMapper.convertValue(challenge, Map.class);
-		Message message = Message.builder()
-			.putAllData(challengeMap)
-			.setToken(devicePushId)
-			.build();
-
-		try {
-			String response = FirebaseMessaging.getInstance().send(message);
-			logger.debugv("Successfully sent message: ", response);
-		} catch (FirebaseMessagingException e) {
-			logger.error("Failed to send firebase app notification", e);
+		PubClient client = new NtfyClient(ClientType.PUB).getClient();
+		NtfyRequest request = new NtfyRequest();
+		request.setHost("ntfy.hackermb.com");
+		request.setTopic(devicePushId);
+		request.setTitle("Keycloak Authentication Notification");
+		request.setMessage("New Login Request");
+		try{
+			client.sendNotification(request);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
